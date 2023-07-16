f074bc348ad2e03f3007b480e05dc2c7beb9a9af
Kilox6991 <4inf.mpromero@gmail.com>
2023-07-16 13:48:25 +0200

    Solucion ejercicio 1

--- src/crud.js
+++ src/crud.js
@@ -1,14 +1,29 @@
 const Book = require('./book');
 
-const getBookById = (id) => {};
+const getBookById = (id) => {
+		const book = Book.findById(id);
+		return book;
+};
 
-const getAllBooks = () => {};
+const getAllBooks = () => {
+	const books = Book.find({});
+    return books;
+};
 
-const createBook = (bookDetails) => {};
+const createBook = (bookDetails) => {
+	const book = Book.create(bookDetails);
+    return book;
+};
 
-const removeBookById = (id) => {};
+const removeBookById = (id) => {
+	const removedBook = Book.findByIdAndRemove(id);
+    return removedBook;
+};
 
-const updateBookById = (id, update) => {};
+const updateBookById = (id, update) => {
+	const updatedBook = Book.findByIdAndUpdate(id, update, { new: true });
+    return updatedBook;
+};
 
 module.exports = {
 	getBookById,
