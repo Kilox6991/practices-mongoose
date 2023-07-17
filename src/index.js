7717c3f1030dcefecfbba1a075ca58d54335f339
Kilox6991 <4inf.mpromero@gmail.com>
2023-07-17 11:05:28 +0200

    Solucion ejercicio 5

--- src/index.js
+++ src/index.js
@@ -13,17 +13,34 @@ app.use(express.json());
 
 app.get('/todo/:id', async (req, res) => {
 	const todoId = req.params.id;
+	const todo = await Todo.findById(todoId);
+	
 });
 
-app.get('/todos', async (req, res) => {});
+app.get('/todos', async (req, res) => {
+	const todos = await Todo.find({});
+	res.json(todos); 
+});
 
 app.post('/todo', async (req, res) => {
 	const todoToCreate = req.body.todo;
+	const createTodo = await Todo.create(todoToCreate);
+	res.json(createTodo)
 });
 
-app.put('/todo/:id', async (req, res) => {});
+app.put('/todo/:id', async (req, res) => {
+	const todoId = req.params.id;
+	const todoUpdate = req.body.todo; 
+	const update = await Todo.findByIdAndUpdate(todoId, todoUpdate, {})
+	res.json(update)
+});
 
-app.delete('/todo/:id', async (req, res) => {});
+app.delete('/todo/:id', async (req, res) => {
+	const todoId = req.params.id;
+	const deleteTodo = await Todo.findByIdAndRemove(todoId);
+	
+	res.json(deleteTodo);
+});
 
 async function main() {
 	app.listen(9000, () => console.log('server on'));
